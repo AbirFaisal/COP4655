c543081fea9e8e4fb63a69ff7782fe6479605698
Abir Faisal <example@example.com>
2024-10-15 09:15:50 -0400

    Update Project 3

---
+++ Project3_BeRealCloneP2/project-bereal-clone-p2/Feed/AddCommentCell.swift
@@ -0,0 +1,31 @@
+//
+//  AddCommentCell.swift
+//  project-bereal-clone-p2
+//
+//  Created by Administrator on 10/15/24.
+//
+
+import Foundation
+import UIKit
+
+
+
+class AddCommentCell: UITableViewCell {
+
+
+    @IBOutlet weak var addCommentTextField: UITextField!
+
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+
+
+
+}
